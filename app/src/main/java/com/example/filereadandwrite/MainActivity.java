3bfbb12d5d7f58b992dee0d0fd5364466220a118
zhao-xudong <773626743@qq.com>
2018-04-23 22:33:37 +0800

    内部存储文件操作

---
+++ app/src/main/java/com/example/filereadandwrite/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.filereadandwrite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String MyFileName="NewFile";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnWrite=(Button)findViewById(R.id.ButtonWrite);
+        Button btnRead=(Button)findViewById(R.id.ButtonRead);
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OutputStream out=null;
+                try {
+                    FileOutputStream fileOutputStream=openFileOutput(MyFileName,MODE_PRIVATE);
+                    out=new BufferedOutputStream(fileOutputStream);
+                    String number="number:2015011417 ";
+                    String name="name:ZhaoXuDong";
+                    try {
+                        out.write(number.getBytes());
+                        out.write(name.getBytes());
+                    }
+                    finally {
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream in=null;
+                try {
+                    FileInputStream fileInputStream = openFileInput(MyFileName);
+                    in=new BufferedInputStream(fileInputStream);
+
+                    int c;
+                    StringBuilder stringBuilder=new StringBuilder("");
+                    try{
+                        while ((c=in.read())!=-1) {
+                            stringBuilder.append((char)c);
+                        }
+                        Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                    }
+                    finally {
+                        if(in!=null)
+                            in.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
